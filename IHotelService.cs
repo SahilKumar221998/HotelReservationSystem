652a5e21fe485bce0084291aa2d4e38e67a92a67
SahilKumar221998 <sahilja265@gmail.com>
2024-06-11 14:14:34 +0530

    Interface with addHotel abstract method

---
+++ IHotelService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationSystem
+{
+    public interface IHotelService
+    {
+        public List<Hotels> addHotels();
+    }
+}
