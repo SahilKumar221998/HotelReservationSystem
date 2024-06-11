d690fd7833342b43a927b87472d270ec11aec841
SahilKumar221998 <sahilja265@gmail.com>
2024-06-11 14:16:12 +0530

    Exception class with meaningfull exceptions for date credentials

---
+++ InvalidHotelReservationException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationSystem
+{
+    public class InvalidHotelReservationException:Exception
+    {
+        public InvalidHotelReservationException(string message) : base(message) { 
+        
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
