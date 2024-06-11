e19e53dc3e1b0d08a41db360c9a8f99719604131
SahilKumar221998 <sahilja265@gmail.com>
2024-06-11 14:13:00 +0530

    HotelReservation class with main method

---
+++ HotelReservation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationSystem
+{
+    public class HotelReservation
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("______________________________________________________________________________________________");
+            Console.WriteLine("_____________________________WELCOME TO HOTEL RESERVATION SYSTEM______________________________");
+            Console.WriteLine("----------------------------------------------------------------------------------------------");
+            HotelServiceImpl hotelService = new HotelServiceImpl();
+            hotelService.hotel_Based_OnRange();
+        }
+    }
+}
